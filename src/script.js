f49efe9a058c7b27af6899c5bc7efedb828a81e8
wardahsharif <119267583+wardahsharif@users.noreply.github.com>
2023-06-23 12:00:36 +0300

    added some styling

--- src/script.js
+++ src/script.js
@@ -1,7 +1,40 @@
-function form() {
-let username = document.querySelector("username");
-let password = document.querySelector("password");
+function LoginForm(form) {
+    if( form.userName.value =="username") {
+        if (form.passWord.value =="password")
+     {
+    location=="home.html"
+    }
+    else {
+        alert ("incorrect username or password")
+    }
 }
 
-let button = document.querySelector("button");
-button.addEventListener("click", form);
+}
+
+
+function formBox(form) {
+  let username = document.querySelector('input[name="username"]');
+  let password = document.querySelector('input[name="password"]');
+  username.value = "welcome to the homepage";
+}
+
+let button = document.querySelector("button[type='submit']"); // Get the submit button by type
+button.addEventListener("click", function (event) {
+  event.preventDefault(); // Prevent form from submitting and page reloading
+  formBox();
+});
+
+
+
+
+function formBox(form) {
+  let username = document.querySelector('input[name="username"]');
+  let password = document.querySelector('input[name="password"]');
+  username.value = "welcome to the homepage";
+}
+
+let button = document.querySelector("button[type='submit']");
+button.addEventListener("click", function (event) {
+  event.preventDefault(); 
+  formBox();
+});
